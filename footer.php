7655be243c38f5d3cd80735c4867caada1928d0c
max-yudkin <max.judkin@yandex.com>
2015-07-12 16:14:49 +0300

    proto

---
+++ footer.php
@@ -0,0 +1,22 @@
+<div class="footer">
+  <div class="footer-menu">
+    <ul>
+      <li><a href="#">Каталог</a></li>
+      <li><a href="#">О нас</a></li>
+      <li><a href="#">Опции</a></li>
+      <li><a href="#">Размещение</a></li>
+      <li><a href="#">Дисконт клуб</a></li>
+      <li><a href="#">Статьи</a></li>
+      <li><a href="#">Контакты</a></li>
+    </ul>
+  </div>
+  <div class="footer-copyright">
+      © 2007 ООО "Максимум" Франшиза
+      <ul class="social">
+        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fa fa-vk"></i></a></li>
+        <li><a href="#"><i class="fa fa-facebook-official"></i></a></li>
+        <li><a href="#"><i class="fa fa-google-plus-square"></i></a></li>
+      </ul>
+  </div>
+</div>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Прототип главной страницы</title>
+    <link rel="stylesheet" href="css/theme.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+  </head>
+  <body>
+    <div class="brand-container">
+      <div class="simple-menu">
+        <ul>
+          <li><a href="#">Каталог</a></li>
+          <li><a href="#">О нас</a></li>
+          <li><a href="#">Опции</a></li>
+          <li><a href="#">Размещение</a></li>
+          <li><a href="#">Дисконт клуб</a></li>
+          <li><a href="#">Статьи</a></li>
+          <li><a href="#">Контакты</a></li>
+        </ul>
+      </div>
+
+      <div class="start-search">
+        <div class="logo-box">
+          <img src="http://maksimum.info/wp-content/uploads/2015/02/%D0%BB%D0%BE%D0%B3%D0%BE2.png">
+        </div>
+        <h1>
+          «Городская справочная служба Москва»
+        </h1>
+        <div class="search-box">
+            <input type="text">
+            <button>Поиск</button>
+        </div>
+        <div class="search-info">
+          Бесплатная справочная по товарам и услугам.
+        </div>
+      </div>
+    </div>
+
+    <?php include('footer.php') ?>
+  </body>
+</html>
